93b01c665a133fe6edf47ca86ff03d5025199f91
Dave Copeland <dave.copeland@opower.com>
2011-05-03 20:30:29 -0400

    reasonably complete logger implementation

--- lib/methadone/cli_logger.rb
+++ lib/methadone/cli_logger.rb
@@ -1,23 +1,65 @@
 require 'logger'
 
 module Methadone
+  # A Logger appropriate for a command-line program in that it logs
+  # all messages (based on level) to the standard output and logs "error" type
+  # messages additioanlly to the standard error.  By default, this will pretty
+  # much do what you want, however it can be customized:
+  # 
+  # * You can override the devices used by passing different devices to the constructor
+  # * You can adjust the level of message that goes to the error logger via error_level=
+  # * You can adjust the format for messages to the error logger separately via error_formatter=
+  #
+  # Example
+  #
+  #     logger = CLILogger.new
+  #     logger.debug("Starting up") # => only the standard output gets this
+  #     logger.error("Something went wrong!") # => both standard error AND standard output get this
   class CLILogger < Logger
 
-    def initialize(log_device=$stdout,error_device=$stderr)
-      super(log_device)
-      @stderr_logger = Logger.new(error_device)
-    end
-
-    def add(severity, message = nil, progname = nil, &block)
-      super(severity,message,progname,&block)
-      if severity >= Logger::Severity::WARN
-        @stderr_logger.add(severity,message,progname,&block)
+    # Helper to proxy methods to the super class AND to the internal error logger
+    # 
+    # +symbol+:: Symbol for name of the method to proxy
+    def self.proxy_method(symbol) #:nodoc:
+      old_name = "old_#{symbol}".to_sym
+      alias_method old_name,symbol
+      define_method symbol do |*args,&block|
+        send(old_name,*args,&block)
+        @stderr_logger.send(symbol,*args,&block)
       end
     end
 
-    def formatter=(formatter)
-      super(formatter)
-      @stderr_logger.formatter = formatter
+    proxy_method :'formatter='
+    proxy_method :'datetime_format='
+    proxy_method :add
+
+    # A logger that logs error-type messages to a second device; useful
+    # for ensuring that error messages go to standard error
+    #
+    # +log_device+:: device where all log messages should go, based on level
+    # +error_device+:: device where all error messages should go.  By default, this is Logger::Severity::WARN
+    def initialize(log_device=$stdout,error_device=$stderr)
+      super(log_device)
+      @stderr_logger = Logger.new(error_device)
+      @stderr_logger.level = Logger::Severity::WARN
     end
+
+    # Set the threshold for what messages go to the error device.  Note that calling
+    # level= will *not* affect the error logger
+    #
+    # +level+:: a constant from Logger::Severity for the level of message that should go
+    #           to the error logger
+    def error_level=(level)
+      @stderr_logger.level = level
+    end
+
+    # Overrides the formatter for the error logger.  A future call to formatter will
+    # affect both, so order of the calls matters
+    #
+    # +formatter+:: Proc that handles the formatting, the same as for formatter=
+    def error_formatter=(formatter)
+      @stderr_logger.formatter=formatter
+    end
+
   end
 end
--- test/test_cli_logger.rb
+++ test/test_cli_logger.rb
@@ -6,6 +6,9 @@ class TestCLILogger < BaseTest
   include Methadone
   
   def setup
+    @blank_format = proc { |severity,datetime,progname,msg|
+      msg + "\n"
+    }
     @real_stderr = $stderr
     @real_stdout = $stdout
     $stderr = StringIO.new
@@ -17,11 +20,16 @@ class TestCLILogger < BaseTest
     $stdout = @real_stdout
   end
 
-  test "logger sends everything to stdout, and warns, errors, and fatals to stderr" do
+  def logger_with_blank_format
     logger = CLILogger.new
-    logger.formatter = proc { |severity,datetime,progname,msg|
-      msg + "\n"
-    }
+    logger.formatter = @blank_format
+    logger.level = Logger::Severity::DEBUG
+    logger
+  end
+
+  test "logger sends everything to stdout, and warns, errors, and fatals to stderr" do
+    logger = logger_with_blank_format
+
     logger.debug("debug")
     logger.info("info")
     logger.warn("warn")
@@ -30,6 +38,78 @@ class TestCLILogger < BaseTest
 
     $stdout.string.should == "debug\ninfo\nwarn\nerror\nfatal\n"
     $stderr.string.should == "warn\nerror\nfatal\n"
+  end
+
+  test "we can control what goes to stderr" do
+    logger = logger_with_blank_format
+    logger.error_level = Logger::Severity::FATAL
+
+    logger.debug("debug")
+    logger.info("info")
+    logger.warn("warn")
+    logger.error("error")
+    logger.fatal("fatal")
+
+    $stdout.string.should == "debug\ninfo\nwarn\nerror\nfatal\n"
+    $stderr.string.should == "fatal\n"
+  end
+
+  test "we can log to alternate devices easily" do
+    out = StringIO.new
+    err = StringIO.new
+
+    logger = CLILogger.new(out,err)
+    logger.formatter = @blank_format
+
+    logger.debug("debug")
+    logger.info("info")
+    logger.warn("warn")
+    logger.error("error")
+    logger.fatal("fatal")
+
+    out.string.should == "debug\ninfo\nwarn\nerror\nfatal\n"
+    err.string.should == "warn\nerror\nfatal\n"
+  end
+
+
+  test "error logger ignores the log level" do
+    logger = logger_with_blank_format
+    logger.level = Logger::Severity::FATAL
+    logger.debug("debug")
+    logger.info("info")
+    logger.warn("warn")
+    logger.error("error")
+    logger.fatal("fatal")
+
+    $stdout.string.should == "fatal\n"
+    $stderr.string.should == "warn\nerror\nfatal\n"
+  end
+
+  test "both loggers use the same date format" do
+    logger = CLILogger.new
+    logger.datetime_format = "the time"
+    logger.debug("debug")
+    logger.error("error")
+    $stdout.string.should match /the time.*DEBUG.*debug/
+    $stderr.string.should match /the time.*ERROR.*error/
+  end
+
+  test "error logger does not get <<" do
+    logger = logger_with_blank_format
+    logger << "foo"
+    $stdout.string.should == "foo"
+    $stderr.string.should == ""
+  end
+
+  test "error logger can have a different format" do
+    logger = logger_with_blank_format
+    logger.error_formatter = proc { |severity,datetime,progname,msg|
+      "ERROR_LOGGER: #{msg}\n"
+    }
+    logger.debug("debug")
+    logger.error("error")
+    $stdout.string.should == "debug\nerror\n"
+    $stderr.string.should == "ERROR_LOGGER: error\n"
   end
 
 end
