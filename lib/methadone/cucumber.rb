1c373e60e10a7e08e700be8a3bf35f4dba4f48d8
Dave Copeland <davetron5000@gmail.com>
2011-11-03 23:45:19 -0500

    removed deprecated uses of cuke methods

--- lib/methadone/cucumber.rb
+++ lib/methadone/cucumber.rb
@@ -1,25 +1,25 @@
 When /^I get help for "([^"]*)"$/ do |app_name|
   @app_name = app_name
-  When %(I run `#{app_name} --help`)
+  step %(I run `#{app_name} --help`)
 end
 
 Then /^the following options should be documented:$/ do |options|
   options.raw.each do |option|
-    Then %(the option "#{option[0]}" should be documented)
+    step %(the option "#{option[0]}" should be documented)
   end
 end
 
 Then /^the option "([^"]*)" should be documented$/ do |option|
-  Then %(the output should match /^\\s*#{option}\\s+\\w\\w\\w+/)
+  step %(the output should match /^\\s*#{option}\\s+\\w\\w\\w+/)
 end
 
 Then /^the banner should be present$/ do
-  Then %(the output should match /Usage: #{@app_name}/)
+  step %(the output should match /Usage: #{@app_name}/)
 end
 
 Then /^the banner should document that this app takes options$/ do
-  Then %(the output should match /\[options\]/)
-  And %(the output should contain "Options")
+  step %(the output should match /\[options\]/)
+  step %(the output should contain "Options")
 end
 
 Then /^the banner should document that this app's arguments are:$/ do |table|
@@ -27,16 +27,16 @@ Then /^the banner should document that this app's arguments are:$/ do |table|
     option = row[0]
     option = "[#{option}]" if row[1] == 'optional' || row[1] == 'which is optional'
   }.join(' ')
-  Then %(the output should contain "#{expected_arguments}")
+  step %(the output should contain "#{expected_arguments}")
 end
 
 Then /^the banner should document that this app takes no options$/ do
-  Then %(the output should not contain "[options]")
-  And %(the output should not contain "Options")
+  step %(the output should not contain "[options]")
+  step %(the output should not contain "Options")
 end
 
 Then /^the banner should document that this app takes no arguments$/ do
-  Then %(the output should match /Usage: #{@app_name}\\s*$/)
+  step %(the output should match /Usage: #{@app_name}\\s*$/)
 end
 
 Then /^there should be a one line summary of what the app does$/ do
