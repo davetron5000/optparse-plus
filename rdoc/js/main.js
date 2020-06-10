bba3814d6017ccdbeefc4fe4f81806110a8a5f52
Dave Copeland <davec@naildrivin5.com>
2020-06-10 18:43:03 -0400

    rename

--- rdoc/js/main.js
+++ rdoc/js/main.js
@@ -1,5 +1,4 @@
-function toggleSource(id)
-{
+function toggleSource(id) {
   var src = $('#' + id).toggle();
   var isVisible = src.is(':visible');
   $('#l_' + id).html(isVisible ? 'hide' : 'show');
@@ -10,11 +9,17 @@ function toggleSource(id)
 }
 
 window.highlight = function(url) {
-  var hash = url.match(/#([^#]+)$/)
-  if(hash) {
-    $('a[name=' + hash[1] + ']').parent().effect('highlight', {}, 'slow')
+  var hash = url.match(/#([^#]+)$/);
+  if (hash) {
+    var parent = document.querySelector('a[name=' + hash[1] + ']').parentElement;
+
+    parent.classList.add('highlight');
+
+    setTimeout(function() {
+      parent.classList.remove('highlight');
+    }, 1000);
   }
-}
+};
 
 $(function() {
   highlight('#' + location.hash);
